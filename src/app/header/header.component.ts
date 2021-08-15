22acf34a531cca58b0d54789f07b7669bdd65360
vinnie9309 <vgeorgiev1@icloud.com>
2021-08-15 17:57:45 +0300

    adding all files

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  faFilm = faFilm;
+}
